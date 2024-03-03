b00dd7e33930b8fcffa1e1e22adadccaecab31a3
Linda Chen <lchen9049@Lindas-Laptop.local>
2024-03-03 16:35:51 -0600

    update name

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,5 +34,5 @@ resource "azurerm_app_service" "app_service" {
   name                = "tfWebApp"
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_service_plan.app_service_plan.id
 }
