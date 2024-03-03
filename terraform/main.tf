5a9dd1a28045dfd57e96d5ca3cb18560628a6196
Linda Chen <lchen9049@Lindas-Laptop.local>
2024-03-03 15:51:15 -0600

    initial setup

---
+++ terraform/main.tf
@@ -0,0 +1,38 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "myTFResourceGroup"
+  location = "eastus2"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "tf-appserviceplan"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "app_service" {
+  name                = "tf-web-app "
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+}
