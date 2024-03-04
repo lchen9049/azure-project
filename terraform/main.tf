b949cb6a638e18a01108c53c4fd9e3b8144823ee
Linda Chen <lchen9049@gmail.com>
2024-03-03 18:15:47 -0600

    format

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "azurerm" {
-      resource_group_name  = "tfstate"
-      storage_account_name = "tfstate21567"
-      container_name       = "tfstate"
-      key                  = "terraform.tfstate"
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate21567"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 
   required_version = ">= 1.1.0"
